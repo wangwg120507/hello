32100f266e5b86f2ba35f16d6c644151f2013c1a
wangwg120507 <wangwg120507@dhc.com.cn>
2022-06-21 15:57:05 +0800

    コメント

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+  @RequestMapping("/hello")
+  public String hello(Model model) {
+    model.addAttribute("hello", "Hello World!");
+    return "Hello";
+  }
+}
